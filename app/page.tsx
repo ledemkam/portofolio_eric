da851537ff365a67ed47741e9e2b73b4ede47228
ledemkam <ledemkam@gmail.com>
2024-12-04 19:40:18 +0100

    posts implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Intro from "@/components/Intro";
+import RecentPosts from "@/components/RecentPosts";
+import ProjectsPage from "./projects/page";
 
 export default function Home() {
   return (
     <section  className="py-24">
       <div className="container max-w-3xl">
         <Intro />
+        <RecentPosts/>
+        <ProjectsPage/>
       </div>
     </section>
   )
---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default async function ProjectsPage() {
+
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Projects</h1>
+        <p>coming soon .....</p>
+      </div>
+    </section>
+  )
+}
