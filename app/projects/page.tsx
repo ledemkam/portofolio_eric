7d617f3a536e1768a2a53fabada5d066016d570d
ledemkam <ledemkam@gmail.com>
2024-12-05 19:36:07 +0100

    90% fertig

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Projects from '@/components/shared/Projects'
 import { getProjects } from '@/lib/projects'
+import Link from 'next/link'
 
 export default async function ProjectsPage() {
   const projects = await getProjects()
@@ -8,8 +9,8 @@ export default async function ProjectsPage() {
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <h1 className='title mb-12'>Projekten</h1>
-
         <Projects projects={projects} />
+         <p className='mt-6 text-right'>Web-Projekten comming soon, Sie können weitere Projekten <Link href="https://github.com/ledemkam">hier</Link> anschauen</p>
       </div>
     </section>
   )
