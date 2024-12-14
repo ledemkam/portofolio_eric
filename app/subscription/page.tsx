96abf5e1772f6a3f82a734406c48b498c08fa840
ledemkam <ledemkam@gmail.com>
2024-12-14 18:22:07 +0100

    98% finish

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,17 +1,14 @@
-"use client"
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
 
 export default function PageSubscription() {
-    const searchParams = useSearchParams();
-    const email = searchParams.get('email');
+   
   
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
           <h1 className="text-2xl font-bold text-gray-800 mb-4">Abonnement bestätigt !</h1>
           <p className="text-gray-700 mb-6">
-          Vielen Dank für die Bestätigung Ihres Abonnements für unseren Newsletter, <span className="font-semibold">{email}</span> !
+          Vielen Dank für die Bestätigung Ihres Abonnements für unseren Newsletter, <span className="font-semibold">email</span> !
           </p>
           <Link
             href="/"
